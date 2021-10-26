4f6bb1829a319a07845ab32fba4d977853b34c77
ITITIU20226 <91868746+TaKhang@users.noreply.github.com>
2021-10-26 15:00:04 +0700

    Update Exercise4.c

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -50,24 +50,24 @@ void printArray(int a[SIZE][SIZE], int m, int n)
 void insertRow(int arr[], int a[SIZE][SIZE],int rowIndex, int m, int n){
 	//Your codes here
 	
-	int row, column;
+	int row, col;
 	for ( row = m - 1; row >= rowIndex; row--) {
-		for ( column=0; column < n; column++) {
-			a[row + 1][column] = a[row][column];
+		for ( col=0; col < n; col++) {
+			a[row + 1][col] = a[row][col];
 		}
 	} m++;
-	for ( column=0; column < n; column++) {
-		a[rowIndex][column] = arr[column];
+	for ( col=0; col < n; col++) {
+		a[rowIndex][col] = arr[col];
 	}
 	printArray(a, m, n);
 }
 void removeRow(int a[SIZE][SIZE], int rowIndex, int m, int n){
 	//Your codes here
 	
-	int row, column;
+	int row, col;
 	for ( row = rowIndex; row < m; row++) {
-		for ( column=0; column < n; column++) {
-			a[row][column] = a[row + 1][column];
+		for ( col=0; col < n; col++) {
+			a[row][col] = a[row + 1][col];
 		}
 	} m--;
 	printArray(a, m, n);
@@ -75,10 +75,10 @@ void removeRow(int a[SIZE][SIZE], int rowIndex, int m, int n){
 void insertCol(int arr[], int a[SIZE][SIZE],int colIndex, int m, int n){
 	//Your codes here
 	
-	int row, column;
-	for ( column = n - 1; column >= colIndex; row--) {
+	int row, col;
+	for ( col = n - 1; col >= colIndex; row--) {
 		for ( row=0; row < m; row++) {
-			a[row][column + 1] = a[row][column];
+			a[row][col + 1] = a[row][col];
 		}
 	} n++;
 	for ( row=0; row < m; row++) {
@@ -89,10 +89,10 @@ void insertCol(int arr[], int a[SIZE][SIZE],int colIndex, int m, int n){
 void removeCol(int a[SIZE][SIZE], int colIndex, int m, int n){
 	//Your codes here
 	
-	int row, column;
+	int row, col;
 	for ( col = colIndex; col < n; col++) {
 		for ( row=0; row < m; row++) {
-			a[row][column] = a[row][column + 1];
+			a[row][col] = a[row][col + 1];
 		}
 	} m--;
 	printArray(a, m, n);
