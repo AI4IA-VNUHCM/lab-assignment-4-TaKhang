f6e1d7ecc6f44d94ab270c20db3e17bb667335fd
ITITIU20226 <91868746+TaKhang@users.noreply.github.com>
2021-10-26 12:20:11 +0700

    Update Exercise1.c

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -17,6 +17,22 @@ ______________________________________
 
 void Ex1(int n){
 	//Your codes here
+	
+	int pascalTriangle( int colum, int n) {
+	if ( colum == 0 || colum == n ) {
+		return 1;
+	} else {
+		return pascalTriangle( colum - 1, n - 1) + pascalTriangle( colum, n - 1);
+	}
+}
+
+	int row=0, colum=0;
+	for (; row <= n; row++) {
+		for (; colum <= row; colum++) {
+			printf("%2d", pascalTriangle(colum, row));
+		}
+		printf("\n");
+	}  
     
 }
 
